e517fab107d4d920456b91b3ac5e2630a5805d30
lucasregisdemoraes <lucasgrm03@gmail.com>
2023-08-26 16:45:31 -0300

    finish challenge 32

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<h1 align="center">App de tradução de SQL com IA</h1>
 
-## Getting Started
+<p align="center">Desafio #boraCodar 32 da Rocketseat</p>
 
-First, run the development server:
+<p align="center">
+    <a href="https://lucasregisdemoraes.github.io/boracodar">Acesse todos os desafios #BoraCodar concluidos por mim aqui</a>
+</p>
 
+<br>
+
+<p align="center">
+    <img src="./preview.jpg">
+</p>
+
+## Como executar esse projeto
+
+1. Faça o clone desse repositório
+2. Acesse a pasta do projeto
+3. Crie um arquivo `.env.local` e coloque a chave da API da OpenAI nesse formato `OPENAI_API_KEY='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'`
+4. Utilize um dos comandos 
 ```bash
 npm run dev
 # or
@@ -11,24 +25,16 @@ yarn dev
 # or
 pnpm dev
 ```
+5. Abra `http://localhost:3000` no seu navegador para ver o resultado
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Tecnologias
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+Esse projeto foi desenvolvido com as seguintes tecnologias:
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- NextJS
+- TypeScript
+- [Figma](https://www.figma.com)
+- Icones de lucide-react
+- [react-simple-code-editor](https://github.com/react-simple-code-editor/react-simple-code-editor)
+- pirsmjs
+- useCompletion OpenAI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
+'use client'
+
+import { Logo } from '../../public/logo'
+import { Trash2, Stars } from 'lucide-react'
+
+import { useCompletion } from 'ai/react'
+import { useState } from 'react'
+
+import Editor from 'react-simple-code-editor'
+
+import { highlight, languages } from 'prismjs'
+import 'prismjs/components/prism-sql'
+import 'prismjs/themes/prism-dark.css'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [schema, setSchema] = useState('')
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const { completion, input, handleInputChange, handleSubmit } = useCompletion({
+    api: 'api/generate-sql',
+    body: {
+      schema
+    }
+  })
+
+  const result = completion
+
+  return (
+    <div className='max-w-[430px] mx-auto pt-16 pb-4 px-6'>
+      <header className='flex items-center justify-between'>
+        <Logo />
+        <button className='hover:opacity-70 transition-opacity duration-500' title='Apagar' type='button'>
+          <Trash2 className='h-8 w-8 text-snow' strokeWidth={0.8} />
+        </button>
+      </header>
+      <form onSubmit={handleSubmit} className='py-8 w-full flex flex-col text-foam'>
+        <div>
+          <label htmlFor="schema">Cole seu código SQL aqui</label>
+          <Editor
+            textareaId="schema"
+            className='!px-4 !py-3 !overflow-scroll font-mono my-6 h-40 bg-lime-500/5 border border-guava/20 rounded-md focus-within:ring-1 focus-within:ring-lime-600'
+            textareaClassName='!px-4 !py-3 outline-none'
+            value={schema}
+            onValueChange={schema => setSchema(schema)}
+            highlight={schema => highlight(schema, languages.sql, 'sql')}
+          />
+        </div>
+
+        <div className='mt-4'>
+          <label htmlFor="question">Faça uma pergunta sobre o código</label>
+          <textarea
+            className='w-full my-6 bg-lime-500/5 w-100 border border-guava/20 rounded-md px-4 py-3 outline-none focus:ring-1 focus:ring-lime-600'
+            name="question"
+            id="question"
+            value={input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className='px-2 py-3 text-pistachio border-pistachio border rounded-md min-h-14 flex justify-center items-center gap-2 font-semibold hover:opacity-70 transition-opacity duration-500'>
+          <Stars className='w-6 h-6' />
+          Perguntar à inteligência artificial
+        </button>
+      </form>
+      <div className='mt-2'>
+        <span className='text-foam'>Resposta</span>
+        <Editor
+          readOnly
+          textareaId="schema"
+          className='text-foam !px-4 !py-3 !overflow-scroll font-mono my-4 h-40 bg-lime-500/5 border border-guava/20 rounded-md focus-within:ring-1 focus-within:ring-lime-600'
+          textareaClassName='!px-4 !py-3 outline-none'
+          value={result}
+          onValueChange={schema => setSchema(schema)}
+          highlight={schema => highlight(schema, languages.sql, 'sql')}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
